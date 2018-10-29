8e7d277e5fee870e997f3d929ead885324113bc2
mrandrewjbaker <mrandrewjbaker@gmail.com>
2018-10-29 14:06:36 +0000

    V1.0

---
+++ weather.py
@@ -0,0 +1,50 @@
+import requests
+
+
+zip = input('Enter your zip code: ')
+openWeather = 'http://api.openweathermap.org/data/2.5/weather?zip={},us'.format(zip)
+apiKey = '&appid=7751c2d608d40feecf0e2ea229aa371e'
+
+
+
+
+def getWeather(unit):
+
+    url = openWeather + apiKey + unit
+    
+    res = requests.get(url)
+    
+    data = res.json()
+    
+    temp = data['main']['temp']
+    temp = str(temp)
+    
+    city = data['name']
+    
+    print("It is " + temp + ' ' + 'in ' + city)
+
+
+def chooseUnit():
+    queryUnit = input('What unit would you like to use? F or C: ')
+    
+    if queryUnit == 'F' or 'f':
+        unit = '&units=imperial'
+        
+        getWeather(unit)
+        
+    elif queryUnit == 'C' or 'c':
+        unit = '&units=metric'
+        
+        getWeather(unit)
+        
+    else:
+        print('You have entered an invalid unit')
+        chooseUnit()
+
+        
+
+def main():
+    chooseUnit()
+    
+
+main()
